da7dbb2ad88a824b0914d976d015aa66c46280af
calebweldon <caleb.weldon@atscale.com>
2025-04-26 13:31:02 -0500

    first pass spotbugs setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+import com.github.spotbugs.snom.Confidence
+import com.github.spotbugs.snom.Effort
+
 plugins {
+    application
     id("java")
+    checkstyle
+    id("com.github.spotbugs") version "6.0.25"
 }
 
 group = "nu.csse.sqe"
@@ -29,3 +35,38 @@ tasks.compileJava {
 tasks.test {
     useJUnitPlatform()
 }
+
+// Spotbugs README: https://github.com/spotbugs/spotbugs-gradle-plugin#readme
+// SpotBugs Gradle Plugin: https://spotbugs.readthedocs.io/en/latest/gradle.html
+spotbugs {
+    ignoreFailures = false
+    showStackTraces = true
+    showProgress = true
+    effort = Effort.DEFAULT
+    reportLevel = Confidence.DEFAULT
+    //omitVisitors = listOf("FindNonShortCircuit")
+    reportsDir = file("spotbugs")
+    //includeFilter = file("include.xml")
+    //excludeFilter = file("exclude.xml")
+    //onlyAnalyze = listOf("com.foobar.MyClass", "com.foobar.mypkg.*")
+    maxHeapSize = "1g"
+    extraArgs = listOf("-nested:false")
+    //jvmArgs = listOf("-Duser.language=ja") // set user language to japanese
+}
+
+tasks.spotbugsMain {
+    reports.create("html") {
+        required = true
+        outputLocation = layout.buildDirectory.file("reports/spotbugs/spotbugs.html")
+        setStylesheet("fancy-hist.xsl")
+    }
+}
+
+tasks.spotbugsTest {
+    reports.create("html") {
+        required = true
+        outputLocation = file("build/reports/spotbugs/spotbugs_test.html")
+        setStylesheet("fancy-hist.xsl")
+    }
+}
+
