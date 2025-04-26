da0165e532d09c9381fe0ebbc98d2bb06ea718e1
calebweldon <caleb.weldon@atscale.com>
2025-04-26 14:09:03 -0500

    spotbugs + checkstyle added

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,16 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
     // https://mvnrepository.com/artifact/org.easymock/easymock
     testImplementation("org.easymock:easymock:5.4.0")
+
+    // https://docs.gradle.org/current/userguide/checkstyle_plugin.html
+    implementation("com.puppycrawl.tools:checkstyle:10.18.2")
+
+    // https://github.com/spotbugs/spotbugs-gradle-plugin/blob/master/README.md
+    spotbugs("com.github.spotbugs:spotbugs:4.8.6")
+    spotbugsPlugins("com.h3xstream.findsecbugs:findsecbugs-plugin:1.13.0")
 }
 
 java {
@@ -36,6 +44,10 @@ tasks.test {
     useJUnitPlatform()
 }
 
+application {
+    mainClass = "Code.Main" // this may need to change later - I'm basing off template
+}
+
 // Spotbugs README: https://github.com/spotbugs/spotbugs-gradle-plugin#readme
 // SpotBugs Gradle Plugin: https://spotbugs.readthedocs.io/en/latest/gradle.html
 spotbugs {
@@ -47,7 +59,7 @@ spotbugs {
     //omitVisitors = listOf("FindNonShortCircuit")
     reportsDir = file("spotbugs")
     //includeFilter = file("include.xml")
-    //excludeFilter = file("exclude.xml")
+//    excludeFilter = file("config/spotbugs/exclude.xml")
     //onlyAnalyze = listOf("com.foobar.MyClass", "com.foobar.mypkg.*")
     maxHeapSize = "1g"
     extraArgs = listOf("-nested:false")
@@ -68,5 +80,19 @@ tasks.spotbugsTest {
         outputLocation = file("build/reports/spotbugs/spotbugs_test.html")
         setStylesheet("fancy-hist.xsl")
     }
+}
+
+// CheckStyle Gradle Plugin: https://docs.gradle.org/current/userguide/checkstyle_plugin.html
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        xml.required = false
+        html.required = true
+        html.stylesheet = resources.text.fromFile("config/xsl/checkstyle-noframes-severity-sorted.xsl")
+    }
+}
+
+checkstyle{
+    toolVersion = "10.18.2"
+    isIgnoreFailures = false
 }
 
