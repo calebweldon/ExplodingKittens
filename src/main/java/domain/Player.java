502bbf4bed23b525fa8000347fd0017fb6b0e3cd
Golf0ned <benjaminye.email@gmail.com>
2025-05-09 17:56:55 -0500

    delete Player implementation and PlayerTest

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,30 +1,8 @@
 package domain;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Player {
-	private List<Card> hand;
 
-	public Player(List<Card> hand) {
-		this.hand = hand;
-	}
-
-	public void drawCard(Deck deck) {
-		Card drawnCard = deck.drawCard();
-		hand.add(drawnCard);
-	}
-
-	public List<Card> viewHand() {
-		List<Card> copy = List.copyOf(hand);
-		return copy;
-	}
-
-	public Card playCard(int index) {
-		if (index < 0 || index >= hand.size()) {
-			throw new IndexOutOfBoundsException("Invalid index: " + index);
-		}
-
-		return hand.remove(index);
-	}
 }
