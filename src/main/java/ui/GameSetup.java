87a27983838b170c0d131cff5de6762ab7f5795d
Eiko Reisz <eiko.reisz@gmail.com>
2025-05-18 22:00:28 -0500

    Added polymorphism to players and changed HAND_SIZE magic number

--- src/main/java/ui/GameSetup.java
+++ src/main/java/ui/GameSetup.java
@@ -6,11 +6,12 @@ import domain.Player;
 import domain.TurnController;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GameSetup {
 	private final GameController gameController;
 	private final GameView gameView;
-	public static final int HAND_SIZE = 7;
+	public static final int HAND_SIZE = 8;
 
 	public GameSetup() {
 		this.gameView = new GameView();
@@ -18,10 +19,10 @@ public class GameSetup {
 		int numPlayers = this.gameView.chooseNumPlayers();
 		Deck deck = new Deck();
 
-		ArrayList<Player> players = new ArrayList<>(numPlayers);
+		List<Player> players = new ArrayList<>(numPlayers);
 		for (int i = 0; i < numPlayers; i++) {
 			Player player = new Player();
-			for (int j = 0; j < HAND_SIZE; j++) {
+			for (int j = 0; j < HAND_SIZE - 1; j++) {
 				CardType card = deck.drawCard();
 				player.addCard(card);
 			}
