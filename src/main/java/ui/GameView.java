2057ecf5643dbe95b03e4669ce7cde700caf7088
calebweldon <caleb.weldon@atscale.com>
2025-05-22 11:51:01 -0500

    number of players works

--- src/main/java/ui/GameView.java
+++ src/main/java/ui/GameView.java
@@ -7,11 +7,11 @@ import java.util.ResourceBundle;
 import java.util.Locale;
 
 public class GameView {
+	private final Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
 
 	public GameView() { }
 
 	public void chooseLanguage() {
-		Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
 		final String promptForLanguage = "Enter the number of your choice";
 		final String languageOptions  = "1) English\n2) French\n";
 		final String invalidChoice = "Invalid input. Please enter 1 or 2";
@@ -42,7 +42,36 @@ public class GameView {
 	}
 
 	public int chooseNumPlayers() {
-		return 0;
+		Locale locale = LocaleContext.getLocale();
+		ResourceBundle labels = ResourceBundle.getBundle("labels", locale);
+
+		final String promptForNumPlayers = labels.getString("promptForNumPlayers");
+		final String twoPlayersChosen = labels.getString("twoPlayersChosen");
+		final String threePlayersChosen = labels.getString("threePlayersChosen");
+		final String fourPlayersChosen = labels.getString("fourPlayersChosen");
+		final String invalidNumPlayers = labels.getString("invalidNumPlayers");
+
+		System.out.println(promptForNumPlayers);
+
+		final int twoPlayers = 2;
+		final int threePlayers = 3;
+		final int fourPlayers = 4;
+		while (true) {
+			String userInput = scanner.nextLine();
+			switch (userInput) {
+				case "2":
+					System.out.println(twoPlayersChosen);
+					return twoPlayers;
+				case "3":
+					System.out.println(threePlayersChosen);
+					return threePlayers;
+				case "4":
+					System.out.println(fourPlayersChosen);
+					return fourPlayers;
+				default:
+					System.out.println(invalidNumPlayers);
+			}
+		}
 	}
 
 	public void announceGameStart() { }
