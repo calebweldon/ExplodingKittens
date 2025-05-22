bb412be57887901965a9afcec67f61c0710ba3c6
calebweldon <caleb.weldon@atscale.com>
2025-05-22 10:52:12 -0500

    choose language skeleton - checkstyle fails

--- src/main/java/ui/GameView.java
+++ src/main/java/ui/GameView.java
@@ -1,18 +1,42 @@
 package ui;
 
 import domain.Player;
+import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.util.ResourceBundle;
+import java.util.Locale;
 
 public class GameView {
-	private Language language;
 
-	public GameView() {
-		language = Language.ENGLISH;
-	}
+	public GameView() { }
 
-	public void chooseLanguage() { }
+	public void chooseLanguage() {
+		Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
+		final String promptForLanguage = "Enter the number of your choice";
+		final String languageOptions  = "1) English\n2) French\n";
+		final String invalidChoice = "Invalid input. Please enter 1 or 2";
 
-	public Language getLanguage() {
-		return this.language;
+		while (true) {
+			String userInput = scanner.nextLine();
+			switch (userInput) {
+				case "1":
+					LocaleContext.setLocale(new Locale("en"));
+					break;
+				case "2":
+					LocaleContext.setLocale(new Locale("fr"));
+					break;
+				default:
+					System.out.println(invalidChoice);
+					continue;
+			}
+			break;
+		}
+
+		Locale locale = LocaleContext.getLocale();
+		ResourceBundle labels = ResourceBundle.getBundle("labels", locale);
+
+		final String languageSet = labels.getString("language");
+		System.out.println(languageSet);
 	}
 
 	public int chooseNumPlayers() {
--- src/main/java/ui/LocaleContext.java
+++ src/main/java/ui/LocaleContext.java
@@ -3,7 +3,7 @@ package ui;
 import java.util.Locale;
 
 public class LocaleContext {
-	private static Locale currentLocale = Locale.ENGLISH; // default
+	private static Locale currentLocale;
 
 	public static void setLocale(Locale locale) {
 		currentLocale = locale;
