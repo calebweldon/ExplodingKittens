e521a94e32f5aaa33d1b3f52c06da71556704141
calebweldon <caleb.weldon@atscale.com>
2025-05-22 17:49:08 -0500

    handleAttackCardAction fails due to compilation

---
+++ src/test/java/domain/AttackCardControllerTest.java
@@ -0,0 +1,11 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AttackCardControllerTest {
+	@Test
+	public void handleAttackCardAction() {
+		AttackCardController attackCardController = new AttackCardController();
+	}
+}
