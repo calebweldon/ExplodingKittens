4fa455089c7513d44c59b3e0e670b019479dd8fb
calebweldon <caleb.weldon@atscale.com>
2025-05-22 17:37:30 -0500

    test fails due to compilation

---
+++ src/test/java/domain/SkipCardControllerTest.java
@@ -0,0 +1,10 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+public class SkipCardControllerTest {
+	@Test
+	public void handleSkipCardAction() {
+		SkipCardController skipCardController = new SkipCardController();
+	}
+}
