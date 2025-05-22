2aae2c4f0f46480937449b62b07a02daaf28cc94
calebweldon <caleb.weldon@atscale.com>
2025-05-22 11:01:25 -0500

    fixed sptobug error

--- src/main/java/ui/LocaleContext.java
+++ src/main/java/ui/LocaleContext.java
@@ -5,6 +5,8 @@ import java.util.Locale;
 public class LocaleContext {
 	private static Locale currentLocale;
 
+	private LocaleContext() { }
+
 	public static void setLocale(Locale locale) {
 		currentLocale = locale;
 	}
--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -2,8 +2,6 @@ package ui;
 
 public class Main {
 	public static void main(String[] args) {
-		LocaleContext localeContext = new LocaleContext();
-
 		GameSetup gameSetup = new GameSetup();
 		gameSetup.runGame();
 	}
