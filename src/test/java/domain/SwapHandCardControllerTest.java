022cd9938cb703713b2442b201e775c92d8a06f8
calebweldon <caleb.weldon@atscale.com>
2025-05-25 18:02:29 -0500

    handleSwapHandCardAction fails due to compilation

---
+++ src/test/java/domain/SwapHandCardControllerTest.java
@@ -0,0 +1,13 @@
+package domain;
+
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SwapHandCardControllerTest {
+	@Test
+	public void handleSwapHandCardAction() {
+		SwapHandCardController swapHandCardController = new SwapHandCardController();
+	}
+}
