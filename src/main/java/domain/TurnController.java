d3bfb0c48e3bcf774de5262a832646bd16ef33d0
Eiko Reisz <eiko.reisz@gmail.com>
2025-05-27 15:16:47 -0500

    Add CardController observer interface

--- src/main/java/domain/TurnController.java
+++ src/main/java/domain/TurnController.java
@@ -1,12 +1,35 @@
 package domain;
 
-public class TurnController {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TurnController {
+	private final List<ObserverCardController> observers = new ArrayList<>();
+	private Player currPlayer;
+
+	// TODO: Replace dummy method
 	public TurnController(Deck deck) {
 	}
 
+	// TODO: Replace dummy method
 	public TurnResult takeTurn(Player player) {
+		this.currPlayer = player;
+		notifyObservers();
 		return TurnResult.CONTINUE;
+	}
+
+	void registerObserver(ObserverCardController controller) {
+		observers.add((ObserverCardController) controller);
+	}
+
+	void unregisterObserver(ObserverCardController controller) {
+		observers.remove((ObserverCardController) controller);
+	}
+
+	private void notifyObservers() {
+		for (ObserverCardController observer : observers) {
+			observer.updatePlayer(currPlayer);
+		}
 	}
 
 }
