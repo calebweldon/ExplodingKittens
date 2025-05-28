56d0f1e3e2ed6fedf2a20d754c1dede7eb6e964e
calebweldon <caleb.weldon@atscale.com>
2025-05-28 12:19:23 -0500

    unit tests complete - added integration test (may be removed later)

--- src/test/java/domain/SwapHandCardControllerTest.java
+++ src/test/java/domain/SwapHandCardControllerTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import ui.GameController;
 import ui.SwapHandCardView;
 
+import javax.smartcardio.Card;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -42,5 +44,71 @@ public class SwapHandCardControllerTest {
 		assertEquals(expected, actual);
 
 		EasyMock.verify(playerOne, playerTwo, currentPlayer, view);
+	}
+
+	@Test
+	public void UnitTest_OneActivePlayerExcludingCurrentWithZeroCards_handleSwapHandCardAction() {
+		Player playerOne = EasyMock.createMock(Player.class);
+		EasyMock.expect(playerOne.getId()).andReturn(1).anyTimes();;
+		EasyMock.expect(playerOne.getHandSize()).andReturn(0).anyTimes();;
+
+		List<Player> activePlayers = new ArrayList<>(List.of(playerOne));
+
+		Player currentPlayer = EasyMock.createMock(Player.class);
+		currentPlayer.swapHandWith(playerOne);
+
+		SwapHandCardView view = EasyMock.createMock(SwapHandCardView.class);
+		view.actionMessage();
+		EasyMock.expect(view.promptForPlayerToSwapWith(activePlayers)).andReturn(playerOne);
+
+		EasyMock.replay(playerOne, currentPlayer, view);
+
+		SwapHandCardController swapHandCardController = new SwapHandCardController(view);
+		swapHandCardController.updatePlayer(currentPlayer);
+		swapHandCardController.updateActivePlayersExcludingCurrent(activePlayers);
+
+		TurnResult actual = swapHandCardController.handleCardAction();
+		TurnResult expected = TurnResult.CONTINUE;
+
+		assertEquals(expected, actual);
+
+		EasyMock.verify(playerOne, currentPlayer, view);
+	}
+
+	@Test
+	public void IntegrationTest_TwoActivePlayersExcludingCurrentWithDiffHandSizes_handSwapped() {
+
+		Player playerOne = new Player(1);
+		Player playerTwo = new Player(2);
+		Player currentPlayer = new Player(3);
+
+		playerTwo.addCard(CardType.ATTACK);
+		currentPlayer.addCard(CardType.SKIP);
+		currentPlayer.addCard(CardType.SKIP);
+
+		List<Player> activePlayers = new ArrayList<>(List.of(playerOne, playerTwo));
+
+		SwapHandCardView view = EasyMock.createMock(SwapHandCardView.class);
+		view.actionMessage();
+		EasyMock.expect(view.promptForPlayerToSwapWith(activePlayers)).andReturn(playerOne);
+
+		EasyMock.replay(view);
+
+		SwapHandCardController controller = new SwapHandCardController(view);
+		controller.updatePlayer(currentPlayer);
+		controller.updateActivePlayersExcludingCurrent(activePlayers);
+
+		assertEquals(2, currentPlayer.getHandSize());
+		assertEquals(0, playerOne.getHandSize());
+
+		TurnResult actual = controller.handleCardAction();
+		TurnResult expected = TurnResult.CONTINUE;
+
+		assertEquals(actual, expected);
+
+		assertEquals(0, currentPlayer.getHandSize());
+		assertEquals(2, playerOne.getHandSize());
+
+		EasyMock.verify(view);
 	}
 }
