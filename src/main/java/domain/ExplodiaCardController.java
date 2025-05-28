5f60fbaa6d3c96c16ec712ba07b4ff01289c5b00
Eiko Reisz <eiko.reisz@gmail.com>
2025-05-28 15:26:22 -0500

    handleExplodiaCardDraw_Continue passes

---
+++ src/main/java/domain/ExplodiaCardController.java
@@ -0,0 +1,41 @@
+package domain;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import ui.ExplodiaCardView;
+
+import java.util.Map;
+
+@SuppressFBWarnings(value = "EI_EXPOSE_REP2", justification = "Player must be shared")
+public class ExplodiaCardController implements CardController, ActionCardController, DrawCardController, TurnObserver {
+	private ExplodiaCardView explodiaCardView;
+	private Player player;
+	private static final int TOTAL_EXPLODIA = 5;
+
+	public ExplodiaCardController(ExplodiaCardView explodiaCardView) {
+		this.explodiaCardView = explodiaCardView;
+	}
+
+	@Override
+	public void updatePlayer(Player currentPlayer) {
+		this.player = currentPlayer;
+	}
+
+	@Override
+	public TurnResult handleCardDraw() {
+		if (player == null) {
+			throw new IllegalStateException("Player not set");
+		}
+		Map<CardType, Integer> hand = player.viewHand();
+		int numExplodia = hand.get(CardType.EXPLODIA);
+
+		explodiaCardView.drawMessage(numExplodia);
+		return TurnResult.CONTINUE;
+	}
+
+	@Override
+	public TurnResult handleCardAction() {
+		// TODO
+		return TurnResult.CONTINUE;
+	}
+
+}
---
+++ src/test/java/domain/ExplodiaCardControllerTest.java
@@ -0,0 +1,35 @@
+package domain;
+
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import ui.ExplodiaCardView;
+import ui.ImplodingFaceDownCardView;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExplodiaCardControllerTest {
+	@ParameterizedTest
+	@ValueSource(ints = {0, 1, 2, 3})
+	public void handleExplodiaCardDraw_Continue(int numExplodia) {
+		Player player = EasyMock.createMock(Player.class);
+		ExplodiaCardView cv = EasyMock.createMock(ExplodiaCardView.class);
+		Map<CardType, Integer> hand = EasyMock.createMock(Map.class);
+		ExplodiaCardController explodiaCardController = new ExplodiaCardController(cv);
+
+		explodiaCardController.updatePlayer(player);
+		EasyMock.expect(player.viewHand()).andReturn(hand);
+		EasyMock.expect(hand.get(CardType.EXPLODIA)).andReturn(numExplodia);
+
+		cv.drawMessage(numExplodia);
+		EasyMock.replay(cv, player, hand);
+
+		TurnResult result = explodiaCardController.handleCardDraw();
+
+		assertEquals(TurnResult.CONTINUE, result);
+		EasyMock.verify(cv, player, hand);
+	}
+}
