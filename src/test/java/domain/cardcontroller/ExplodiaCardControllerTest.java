cff38a1fbf600c435cfdd690cca14476d666f80b
Eiko Reisz <eiko.reisz@gmail.com>
2025-05-30 14:27:29 -0500

    handleExplodiaCardAction_BecomesSkip test passes

--- src/test/java/domain/cardcontroller/ExplodiaCardControllerTest.java
+++ src/test/java/domain/cardcontroller/ExplodiaCardControllerTest.java
@@ -72,16 +72,36 @@ public class ExplodiaCardControllerTest {
 		ArrayList<CardController> cardControllers = EasyMock.createMock(ArrayList.class);
 		SecureRandom rand = EasyMock.createMock(SecureRandom.class);
 		ExplodiaCardView cv = EasyMock.createMock(ExplodiaCardView.class);
-		AttackCardController attackCardController = EasyMock.createMock(AttackCardController.class);
+		AttackCardController controller = EasyMock.createMock(AttackCardController.class);
 		ExplodiaCardController explodiaCardController = new ExplodiaCardController(cv, cardControllers, rand);
 
 		EasyMock.expect(cardControllers.size()).andReturn(NUM_CONTROLLERS);
 		EasyMock.expect(rand.nextInt(NUM_CONTROLLERS)).andReturn(index);
-		EasyMock.expect(cardControllers.get(index)).andReturn(attackCardController);
-		EasyMock.expect(attackCardController.handleCardAction()).andReturn(TurnResult.ATTACK);
+		EasyMock.expect(cardControllers.get(index)).andReturn(controller);
+		EasyMock.expect(controller.handleCardAction()).andReturn(TurnResult.ATTACK);
 
-		EasyMock.replay(cardControllers, rand, cv, attackCardController);
+		EasyMock.replay(cardControllers, rand, cv, controller);
 		explodiaCardController.handleCardAction();
-		EasyMock.verify(cardControllers, rand, cv, attackCardController);
+		EasyMock.verify(cardControllers, rand, cv, controller);
+	}
+
+	@Test
+	public void handleExplodiaCardAction_BecomesSkip() {
+		int index = 0;
+		int NUM_CONTROLLERS = 10;
+		ArrayList<CardController> cardControllers = EasyMock.createMock(ArrayList.class);
+		SecureRandom rand = EasyMock.createMock(SecureRandom.class);
+		ExplodiaCardView cv = EasyMock.createMock(ExplodiaCardView.class);
+		SkipCardController controller = EasyMock.createMock(SkipCardController.class);
+		ExplodiaCardController explodiaCardController = new ExplodiaCardController(cv, cardControllers, rand);
+
+		EasyMock.expect(cardControllers.size()).andReturn(NUM_CONTROLLERS);
+		EasyMock.expect(rand.nextInt(NUM_CONTROLLERS)).andReturn(index);
+		EasyMock.expect(cardControllers.get(index)).andReturn(controller);
+		EasyMock.expect(controller.handleCardAction()).andReturn(TurnResult.SKIP);
+
+		EasyMock.replay(cardControllers, rand, cv, controller);
+		explodiaCardController.handleCardAction();
+		EasyMock.verify(cardControllers, rand, cv, controller);
 	}
 }
