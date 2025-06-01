216df7357f0e757bb695331daef6ad0cefbc7635
Golf0ned <benjaminye.email@gmail.com>
2025-06-01 05:29:15 -0500

    RecycleCardView placeholder values, will be replaced with i18n

--- src/main/java/ui/RecycleCardView.java
+++ src/main/java/ui/RecycleCardView.java
@@ -2,10 +2,10 @@ package ui;
 
 public class RecycleCardView implements CardView {
 	public void getInfo() {
-
+		System.out.println("Add the most recently played card to your hand.");
 	}
 
 	public void actionMessage() {
-
+		System.out.println("Recycle card played.");
 	}
 }
