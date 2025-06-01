ef94d311313172673d3425e2efc7e548beb9fb95
Eiko Reisz <eiko.reisz@gmail.com>
2025-06-01 17:11:31 -0500

    Move Controllers to cardcontroller package

--- src/test/java/domain/cardcontroller/AttackCardControllerTest.java
+++ src/test/java/domain/cardcontroller/AttackCardControllerTest.java
@@ -1,6 +1,6 @@
-package domain;
+package domain.cardcontroller;
 
-import domain.cardcontroller.AttackCardController;
+import domain.TurnResult;
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 import ui.AttackCardView;
