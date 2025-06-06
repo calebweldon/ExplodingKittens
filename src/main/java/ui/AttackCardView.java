70d967526af3188d78c3249721e5b968dbaea1a6
calebweldon <caleb.weldon@atscale.com>
2025-06-06 13:03:39 -0400

    attack card done

--- src/main/java/ui/AttackCardView.java
+++ src/main/java/ui/AttackCardView.java
@@ -1,23 +1,25 @@
 package ui;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class AttackCardView implements CardView {
 	private final Scanner scanner;
+	private ResourceBundle labels;
 
 	public AttackCardView() {
 		this.scanner = new Scanner(System.in, StandardCharsets.UTF_8);
+		this.labels = ResourceBundle.getBundle("labels", LocaleContext.getLocale());
 	}
 
 	public void getInfo() {
-		// TODO: add locale
-		System.out.println("End your turn(s) without drawing and force the " +
-				"next player to take two turns in a row.");
+		final String attackInfo = labels.getString("attackInfo");
+		System.out.println(attackInfo);
 	}
 
 	public void actionMessage() {
-		// TODO: add locale
-		System.out.println("Attack Card played!");
+		final String attackActionMessage = labels.getString("attackActionMessage");
+		System.out.println(attackActionMessage);
 	}
 }
