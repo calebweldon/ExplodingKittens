788e3e6fc908e5c24916b9f95474a95faa793e00
calebweldon <caleb.weldon@atscale.com>
2025-06-06 14:39:50 -0400

    added FlipCardView

--- src/main/java/domain/cardcontroller/FlipCardController.java
+++ src/main/java/domain/cardcontroller/FlipCardController.java
@@ -3,16 +3,20 @@ package domain.cardcontroller;
 import domain.Deck;
 import domain.TurnResult;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import ui.FlipCardView;
 
 @SuppressFBWarnings(value = "EI_EXPOSE_REP2", justification = "Deck must be shared")
 public class FlipCardController implements CardController, ActionCardController {
+	private final FlipCardView view;
 	private final Deck deck;
 
-	public FlipCardController(Deck deck) {
+	public FlipCardController(FlipCardView view, Deck deck) {
+		this.view = view;
 		this.deck = deck;
 	}
 
 	public TurnResult handleCardAction() {
+		view.actionMessage();
 		deck.flipDeck();
 		return TurnResult.CONTINUE;
 	}
