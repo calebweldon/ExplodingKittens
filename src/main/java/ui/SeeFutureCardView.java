e8c0ddd59714f34454f1b6cd2af5650abfdefac0
calebweldon <caleb.weldon@atscale.com>
2025-06-06 13:41:41 -0400

    see future done

--- src/main/java/ui/SeeFutureCardView.java
+++ src/main/java/ui/SeeFutureCardView.java
@@ -2,20 +2,31 @@ package ui;
 
 import domain.CardType;
 
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
 public class SeeFutureCardView implements CardView {
+	private final ResourceBundle labels;
+
+	public SeeFutureCardView() {
+		this.labels = ResourceBundle.getBundle("labels", LocaleContext.getLocale());
+	}
+
 	public void getInfo() {
-		System.out.println("See the top three cards of the deck.");
+		final String seeFutureInfo = labels.getString("seeFutureInfo");
+		System.out.println(seeFutureInfo);
 	}
 
 	public void actionMessage() {
-		System.out.println("You peek at the top of the deck in order.");
+		final String seeFutureActionMessage = labels.getString("seeFutureActionMessage");
+		System.out.println(seeFutureActionMessage);
 	}
 
 	public void displayTopCards(CardType[] topCards) {
 		int count = 1;
 		for (CardType card : topCards) {
-			String cardString = String.format("{0}: {1}", count, card);
-			System.out.println(card);
+			String cardString = MessageFormat.format("{0}: {1}", count, card);
+			System.out.println(cardString);
 			count++;
 		}
 	}
