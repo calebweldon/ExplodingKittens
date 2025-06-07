9b3d7addb57b99495a9c9d133aece0092d4b979c
Eiko Reisz <eiko.reisz@gmail.com>
2025-06-06 19:11:59 -0500

    Update card controller initializations

--- src/main/java/ui/GameSetup.java
+++ src/main/java/ui/GameSetup.java
@@ -3,6 +3,7 @@ package ui;
 import domain.*;
 import domain.cardcontroller.*;
 
+import javax.smartcardio.Card;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -34,35 +35,78 @@ public class GameSetup {
 
 		TurnView turnView = new TurnView();
 		Map<CardType, CardController> cardControllers = new HashMap<>();
-		cardControllers.put(CardType.ALTER_THE_FUTURE,
-				new AlterFutureCardController(new AlterFutureCardView(), deck));
-		cardControllers.put(CardType.ATTACK,
-				new AttackCardController(new AttackCardView()));
-		// TODO: Add Basic
-		// TODO: Add Embarrass
-		// TODO: Add explodia - intakes list of card controllers
-		// TODO: Add Favor
-		cardControllers.put(CardType.REVERSE,
-				new FlipCardController(new FlipCardView(), deck));
-		// TODO: Add GodCat - intakes map of card controllers
-		cardControllers.put(CardType.IMPLODING_FACEDOWN,
-				new ImplodingFaceDownCardController(
-						new ImplodingFaceDownCardView(), deck));
-		cardControllers.put(CardType.IMPLODING_FACEUP,
-				new ImplodingFaceUpCardController(new ImplodingFaceUpCardView()));
-		// TODO: Add Recycle
-		cardControllers.put(CardType.SEE_THE_FUTURE,
-				new SeeFutureCardController(new SeeFutureCardView(), deck));
-		cardControllers.put(CardType.SHUFFLE,
-				new ShuffleCardController(new ShuffleCardView(), deck));
-		cardControllers.put(CardType.SKIP,
-				new SkipCardController(new SkipCardView()));
-		cardControllers.put(CardType.SWAP,
-				new SwapHandCardController(new SwapHandCardView()));
+
+		CardController alterController = new AlterFutureCardController(
+				new AlterFutureCardView(), deck);
+		CardController attackController = new AttackCardController(
+				new AttackCardView());
+		// TODO: Update Basic
+		CardController basicController = new BasicCardController();
+		// TODO: Update Embarrass
+		CardController embarrassController = new EmbarrassCardController();
+		// TODO: Update Favor
+		CardController favorController = new FavorCardController();
+		CardController flipController = new FlipCardController(
+				new FlipCardView(), deck);
+		CardController implodingDownController = new ImplodingFaceDownCardController(
+				new ImplodingFaceDownCardView(), deck);
+		CardController implodingUpController = new ImplodingFaceUpCardController(
+				new ImplodingFaceUpCardView());
+		// TODO: Update Recycle
+		CardController recycleController = new RecycleCardController();
+		CardController seeController = new SeeFutureCardController(
+				new SeeFutureCardView(), deck);
+		CardController shuffleController = new ShuffleCardController(
+				new ShuffleCardView(), deck);
+		CardController skipController = new SkipCardController(
+				new SkipCardView());
+		CardController swapController = new SwapHandCardController(
+				new SwapHandCardView());
+
+		Map<CardType, CardController> godCatMap = new HashMap<>();
+		godCatMap.put(CardType.ATTACK, attackController);
+		godCatMap.put(CardType.SKIP, skipController);
+		godCatMap.put(CardType.FAVOR, favorController);
+		godCatMap.put(CardType.TACO_CAT, basicController);
+		godCatMap.put(CardType.REVERSE, flipController);
+		godCatMap.put(CardType.SHUFFLE, shuffleController);
+		godCatMap.put(CardType.SWAP, swapController);
+		godCatMap.put(CardType.EMBARRASS, embarrassController);
+		godCatMap.put(CardType.RECYCLE, recycleController);
+		godCatMap.put(CardType.ALTER_THE_FUTURE, alterController);
+		godCatMap.put(CardType.SEE_THE_FUTURE, seeController);
+
+		CardController godCatController = new GodCatCardController(
+				new GodCatCardView(), godCatMap);
+		CardController explodiaController = new ExplodiaCardController(
+				new ExplodiaCardView(), new ArrayList<>(godCatMap.values()));
+
+
+		cardControllers.put(CardType.ALTER_THE_FUTURE, alterController);
+		cardControllers.put(CardType.ATTACK, attackController);
+		cardControllers.put(CardType.BEARD_CAT, basicController);
+		cardControllers.put(CardType.CATTERMELON, basicController);
+		cardControllers.put(CardType.POTATO_CAT, basicController);
+		cardControllers.put(CardType.RAINBOW_RALPHING_CAT, basicController);
+		cardControllers.put(CardType.TACO_CAT, basicController);
+		cardControllers.put(CardType.EMBARRASS, embarrassController);
+		cardControllers.put(CardType.EXPLODIA, explodiaController);
+		cardControllers.put(CardType.FAVOR, favorController);
+		cardControllers.put(CardType.REVERSE, flipController);
+		cardControllers.put(CardType.GOD_CAT, godCatController);
+		cardControllers.put(CardType.IMPLODING_FACEDOWN, implodingDownController);
+		cardControllers.put(CardType.IMPLODING_FACEUP, implodingUpController);
+		cardControllers.put(CardType.RECYCLE, recycleController);
+		cardControllers.put(CardType.SEE_THE_FUTURE, seeController);
+		cardControllers.put(CardType.SHUFFLE, shuffleController);
+		cardControllers.put(CardType.SKIP, skipController);
+		cardControllers.put(CardType.SWAP, swapController);
+
 
 		TurnController turnController = new TurnController(deck, turnView, cardControllers);
 
-		this.gameController = new GameController(players, turnController, this.gameView);
+		this.gameController = new GameController(
+				players, turnController, this.gameView, cardControllers);
 	}
 
 	public void runGame() {
