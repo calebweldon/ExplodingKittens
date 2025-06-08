51ffd6a61ae3e2bf1f2e25ca237df8a8f126a081
Benjamin Ye <benjaminye.email@gmail.com>
2025-06-07 20:13:55 -0500

    move RecycleCardController to cardController/

--- src/main/java/domain/cardcontroller/RecycleCardController.java
+++ src/main/java/domain/cardcontroller/RecycleCardController.java
@@ -1,11 +1,31 @@
-package domain.cardcontroller;
+package domain;
 
-import domain.TurnResult;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import ui.RecycleCardView;
 
-public class RecycleCardController implements CardController, ActionCardController {
-	@Override
+public class RecycleCardController implements CardController, ActionCardController,
+		TurnObserver, LastPlayedObserver {
+	private final RecycleCardView view;
+	@SuppressFBWarnings(value = "EI_EXPOSE_REP2", justification = "player must be shared")
+	private Player currentPlayer;
+	private CardType lastCard;
+
+	public RecycleCardController(RecycleCardView view) {
+		this.view = view;
+		this.lastCard = CardType.RECYCLE;
+	}
+
 	public TurnResult handleCardAction() {
-		// TODO
-		return null;
+		currentPlayer.addCard(lastCard);
+		view.actionMessage();
+		return TurnResult.CONTINUE;
+	}
+
+	public void updatePlayer(Player currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+
+	public void updateLastPlayed(CardType lastPlayed) {
+		this.lastCard = lastPlayed;
 	}
 }
