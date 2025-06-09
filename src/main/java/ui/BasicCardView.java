a6393fe3102fde7c58f14f450dd743bee136b245
Golf0ned <benjaminye.email@gmail.com>
2025-06-09 16:53:35 -0500

    finish BasicCardView

--- src/main/java/ui/BasicCardView.java
+++ src/main/java/ui/BasicCardView.java
@@ -2,6 +2,9 @@ package ui;
 
 import java.util.ResourceBundle;
 import java.util.List;
+import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
 
 import domain.CardType;
 import domain.Player;
@@ -9,35 +12,56 @@ import domain.Player;
 public class BasicCardView implements CardView {
 	private final ResourceBundle labels;
 	private final CardType basicCardType;
+	private final Scanner scanner;
 
 	public BasicCardView(CardType basicCardType) {
 		this.labels = ResourceBundle.getBundle("labels", LocaleContext.getLocale());
 		this.basicCardType = basicCardType;
+		this.scanner = new Scanner(System.in, StandardCharsets.UTF_8);
 	}
 
 	public void getInfo() {
-		final String basicCardInfo = labels.getString("basicCardInfo");
+		final String basicCardInfo = labels.getString("basicInfo");
 		System.out.println(basicCardInfo);
 	}
 
 	public void actionMessage() {
-		final String basicCardActionMessage = labels.getString("basicCardActionMessage");
+		final String basicCardActionMessage = labels.getString("basicActionMessage");
 		System.out.println(basicCardActionMessage);
 	}
 
 	public Player promptForPlayer(List<Player> activePlayersExcludingCurrent) {
-		// TODO
-		return activePlayersExcludingCurrent.get(0);
+		for (Player player : activePlayersExcludingCurrent) {
+			int playerId = player.getId();
+			String playerListing = MessageFormat.format(
+				labels.getString("basicPlayerList"), playerId);
+			System.out.println(playerListing);
+		}
+
+		final String invalidPlayer = labels.getString("basicPlayerInvalidPlayer");
+		while (true) {
+			try {
+				int userInput = Integer.parseInt(scanner.nextLine());
+				for (Player player : activePlayersExcludingCurrent) {
+					if (player.getId() == userInput) {
+						return player;
+					}
+				}
+				System.out.println(invalidPlayer);
+			} catch (NumberFormatException e) {
+				System.out.println(invalidPlayer);
+			}
+		}
 	}
 
 	public void displayOtherPlayerNoCards() {
 		final String otherPlayerNoCardsMessage = labels.getString
-			("otherPlayerNoCardsMessage");
+			("basicOtherPlayerNoCards");
 		System.out.println(otherPlayerNoCardsMessage);
 	}
 
 	public void displayCardTaken(CardType card) {
-		final String cardTakenMessage = labels.getString("cardTakenMessage");
+		final String cardTakenMessage = labels.getString("basicCardTakenMessage");
 		System.out.printf(cardTakenMessage, card);
 	}
 }
